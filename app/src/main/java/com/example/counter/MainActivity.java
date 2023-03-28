c115133dd96d0e340df7912cd016695e5317a272
Dasha-Golubeva <darya.golubeva.2006@mail.ru>
2023-03-28 14:52:44 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.counter;
+
+import static androidx.core.content.PackageManagerCompat.LOG_TAG;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    private TextView textCount;
+
+
+    private int count = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textCount = findViewById(R.id.textCount);
+        button = findViewById(R.id.button);
+    }
+
+    public void onClick(View view) {
+        count++;
+       textCount.setText(Integer.toString(count));
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast toast = Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast toast = Toast.makeText(getApplicationContext(), "onDestroy()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast toast = Toast.makeText(getApplicationContext(), "onStop()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast toast = Toast.makeText(getApplicationContext(), "onPause()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast toast = Toast.makeText(getApplicationContext(), "onResume()", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("saved");
+        Toast toast = Toast.makeText(getApplicationContext(), "onRestoreInstanceState", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("saved", count);
+        Toast toast = Toast.makeText(getApplicationContext(), "onSaveInstanceState", Toast.LENGTH_SHORT);
+        toast.show();
+
+    }
+
+
+}
